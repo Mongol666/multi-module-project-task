59206078868d52764c74b197221041b87e935477
Mongol666 <pisarevgerman78@gmail.com>
2021-09-27 17:18:38 +0300

    create module "models"; initialize classes of this module; some changes in root pom.xml

---
+++ models/src/main/java/by/example/models/Student.java
@@ -0,0 +1,41 @@
+package by.example.models;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+
+public class Student {
+    private BigDecimal[] marks;
+
+    public Student() {
+    }
+
+    public Student(BigDecimal[] marks) {
+        this.marks = marks;
+    }
+
+    public BigDecimal[] getMarks() {
+        return marks;
+    }
+
+    public void setMarks(BigDecimal[] marks) {
+        this.marks = marks;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Arrays.equals(marks, student.marks);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(marks);
+    }
+
+    @Override
+    public String toString() {
+        return "Оценки этого студента: " + Arrays.toString(marks);
+    }
+}
